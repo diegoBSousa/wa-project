62ab446ca2bd4cd8f07e40e096980fb82ff7352b
Diego Brito Sousa <diego@diegosousa.com.br>
2021-08-12 22:47:12 -0300

    Configura campos de data

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,6 @@
 import { Sequelize, Model } from 'sequelize';
+import { now } from 'sequelize/types/lib/utils';
+import { v4 as uuidv4 } from 'uuid';
 
 class User extends Model {
   static init(sequelize) {
@@ -34,6 +36,18 @@ class User extends Model {
         underscored: true,
       }
     );
+
+    this.addHook('beforeCreate', async (user) => {
+      user.uuid = uuidv4();
+      user.updated_at = now;
+      user.created_at = now;
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      user.updated_at = now;
+    });
+
+    return this;
   }
 }
 
