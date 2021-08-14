debbd70cbf2a90350927560cf246a74a80342d20
Diego Sousa <diego@diegosousa.com.br>
2021-08-14 19:28:05 -0300

    Inclui model User no User controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,33 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
 class UserController {
   async store(req, res) {
-    return res.json({ message: 'ok' });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation has failed.' });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { uuid, name, email } = await User.create(req.body);
+
+    return res.json({
+      uuid,
+      name,
+      email,
+    });
   }
 }
 
