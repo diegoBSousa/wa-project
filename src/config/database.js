0f4c889cab474481b16845045f66e3e787966a78
Diego Sousa <diego@diegosousa.com.br>
2021-08-13 22:01:03 -0300

    Configura Postgresql

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
