6a2ef1bd03a8d1813463cc7fdfe1d2564bb657dc
Diego Sousa <diego@diegosousa.com.br>
2021-08-15 12:39:49 -0300

    Cria rota para upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import FileController from './app/controllers/FileController';
 import LaboratoryController from './app/controllers/LaboratoryController';
 import LoginController from './app/controllers/LoginController';
 import UserController from './app/controllers/UserController';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const updload = multer(multerConfig);
 
 routes.get('/', (req, res) => res.json({ message: 'Funciona' }));
 
@@ -23,7 +27,12 @@ routes.get('/users/', UserController.index);
  */
 routes.post('/laboratory/', LaboratoryController.store);
 routes.put('/laboratory/:uuid/', LaboratoryController.update);
-routes.delete('/laboratory/:uuid', LaboratoryController.delete);
-routes.get('/laboratory', LaboratoryController.index);
+routes.delete('/laboratory/:uuid/', LaboratoryController.delete);
+routes.get('/laboratory/', LaboratoryController.index);
+
+/**
+ * Files
+ */
+routes.post('/files/', updload.single('file'), FileController.store);
 
 export default routes;
