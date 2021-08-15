feaf9cfd34b833b24191803483248418cfc097d6
Diego Sousa <diego@diegosousa.com.br>
2021-08-15 17:52:17 -0300

    Adiciona método de atualização de exame

--- src/app/controllers/ExamController.js
+++ src/app/controllers/ExamController.js
@@ -26,6 +26,38 @@ class ExamController {
 
     return res.json(exam);
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string(),
+      tipo: Yup.mixed().oneOf(['análise clínica', 'imagem']),
+      status: Yup.mixed().oneOf(['ativo', 'inativo']),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation has failed' });
+    }
+
+    const { nome } = req.body;
+
+    const nameExists = await Exam.findOne({ where: { nome } });
+    if (nameExists) {
+      return res.status(400).json({
+        error: `This Exam Name: '${nome}' is already in use by another exam.`,
+      });
+    }
+
+    const exam = await Exam.findByPk(req.params.uuid);
+    if (!exam) {
+      return res.status(400).json({
+        error: `There is not a exam with this ID: ${req.params.uuid}`,
+      });
+    }
+
+    await exam.update(req.body);
+
+    return res.json(exam);
+  }
 }
 
 export default new ExamController();
