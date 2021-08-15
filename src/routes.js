b785571678174408c4c570ee55b5b762b8ba8d1b
Diego Sousa <diego@diegosousa.com.br>
2021-08-14 21:33:16 -0300

    Adiciona rota CREATE para Laboratory

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import LaboratoryController from './app/controllers/LaboratoryController';
 import LoginController from './app/controllers/LoginController';
 import UserController from './app/controllers/UserController';
 
@@ -15,5 +16,11 @@ routes.post('/login/', LoginController.store);
  *  User
  */
 routes.post('/users/', UserController.store);
+routes.get('/users', UserController.index);
+
+/**
+ *  Laboratory
+ */
+routes.post('/laboratory', LaboratoryController.store);
 
 export default routes;
