5585269d08acb04475db1d6f379543ca816ebf63
Diego Sousa <diego@diegosousa.com.br>
2021-08-15 10:27:15 -0300

    Cria método para atualizar laboratórios

--- src/app/controllers/LaboratoryController.js
+++ src/app/controllers/LaboratoryController.js
@@ -35,6 +35,31 @@ class LaboratoryController {
       updated_at,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string().required(),
+      endereco_linha_1: Yup.string().required(),
+      endereco_linha_2: Yup.string(),
+      endereco_linha_3: Yup.string(),
+      status: Yup.boolean(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation has failed' });
+    }
+
+    const laboratory = await Laboratory.findByPk(req.params.uuid);
+    if (!laboratory) {
+      return res.status(400).json({
+        error: `There is not any user with this ID: ${req.params.uuid}`,
+      });
+    }
+
+    await laboratory.update(req.body);
+
+    return res.json(laboratory);
+  }
 }
 
 export default new LaboratoryController();
