df6948aae11fe789fa7974f44e624c4af31b656e
Diego Sousa <diego@diegosousa.com.br>
2021-08-15 10:03:18 -0300

    Cria migração da tabela laboratórios

---
+++ src/database/migrations/20210815123933-create-laboratory.js
@@ -0,0 +1,50 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable('laboratories', {
+      uuid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      endereco_linha_1: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      endereco_linha_2: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      endereco_linha_3: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('laboratories');
+  },
+};
