bda12ce5db0325f66225d2ad8b035d54779b3a55
Diego Sousa <diego@diegosousa.com.br>
2021-08-15 09:29:23 -0300

    Cria corpo do método de criação de laboratório

--- src/app/controllers/LaboratoryController.js
+++ src/app/controllers/LaboratoryController.js
@@ -1,9 +1,39 @@
+import * as Yup from 'yup';
 import Laboratory from '../models/Laboratory';
 
 class LaboratoryController {
   async store(req, res) {
-    const laboratory = Laboratory.create();
-    return res.json(laboratory);
+    const schema = Yup.object().shape({
+      nome: Yup.string().required(),
+      endereco_linha_1: Yup.string().required(),
+      endereco_linha_2: Yup.string(),
+      endereco_linha_3: Yup.string(),
+      status: Yup.boolean(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation has failed' });
+    }
+
+    const {
+      uuid,
+      nome,
+      endereco_linha_1,
+      endereco_linha_2,
+      endereco_linha_3,
+      status,
+      updated_at,
+    } = await Laboratory.create(req.body);
+
+    return res.json({
+      uuid,
+      nome,
+      endereco_linha_1,
+      endereco_linha_2,
+      endereco_linha_3,
+      status,
+      updated_at,
+    });
   }
 }
 
